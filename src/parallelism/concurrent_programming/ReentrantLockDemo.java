9dc88aba54577d4bb8fd6b6b2d9839ebf160d1d8
mohammad <m_vahidalizadeh@live.com>
2020-08-11 12:05:07 -0400

    Adding codes related to different locks.

---
+++ src/parallelism/concurrent_programming/ReentrantLockDemo.java
@@ -0,0 +1,44 @@
+package parallelism.concurrent_programming;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantLockDemo {
+
+    static class Shopper extends Thread {
+        static int garlicCount = 0, potatoCount = 0;
+        static ReentrantLock pencil = new ReentrantLock();
+
+        private static void addGarlic() {
+            pencil.lock();
+            System.out.println("Hold count: " + pencil.getHoldCount());
+            garlicCount++;
+            pencil.unlock();
+        }
+
+        private static void addPotato() {
+            pencil.lock();
+            potatoCount++;
+            addGarlic();
+            pencil.unlock();
+        }
+
+        public void run() {
+            for (int i = 0; i < 10_000; i++) {
+                addGarlic();
+                addPotato();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread mohammad = new Shopper();
+        Thread mahnaz = new Shopper();
+        mohammad.start();
+        mahnaz.start();
+        mohammad.join();
+        mahnaz.join();
+        System.out.println("We should buy " + Shopper.garlicCount + " garlic.");
+        System.out.println("We should buy " + Shopper.potatoCount + " potatoes.");
+    }
+
+}
---
+++ src/parallelism/concurrent_programming/TryLockDemo.java
@@ -0,0 +1,58 @@
+package parallelism.concurrent_programming;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class TryLockDemo {
+
+    static class Shopper extends Thread {
+
+        private int itemsToAdd = 0;
+
+        private static int itemsOnNotepad = 0;
+        private static Lock pencil = new ReentrantLock();
+
+        public Shopper(String name) {
+            this.setName(name);
+        }
+
+        public void run() {
+            while (itemsOnNotepad <= 20) {
+                if ((itemsToAdd > 0) && pencil.tryLock()) {
+                    try {
+                        itemsOnNotepad += itemsToAdd;
+                        System.out.println(this.getName() + " added " + itemsToAdd + " item(s) to notepad.");
+                        itemsToAdd = 0;
+                        Thread.sleep(300);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } finally {
+                        pencil.unlock();
+                    }
+                } else {
+                    try {
+                        Thread.sleep(100);
+                        itemsToAdd++;
+                        System.out.println(this.getName() + " found something to buy.");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread mohammad = new Shopper("Mohammad");
+        Thread mahnaz = new Shopper("Mahnaz");
+        long start = System.currentTimeMillis();
+        mohammad.start();
+        mahnaz.start();
+        mohammad.join();
+        mahnaz.join();
+        long finish = System.currentTimeMillis();
+        System.out.println("Elapsed time: " + (float) (finish - start) / 1000 + " seconds");
+    }
+
+}
